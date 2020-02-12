d742397ab6233043985c8a545cfbf7a73d39bd17
David A <david1906@outlook.com>
2020-02-11 20:39:22 -0600

    Updates

---
+++ precache-manifest.24df87ff2e41d2f16437d9ad54c78c83.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4305215b8bd25f7bdcf7d75932349975",
+    "url": "/cardDealer/index.html"
+  },
+  {
+    "revision": "ad4ea105d31618343935",
+    "url": "/cardDealer/static/css/main.b9fc7fa2.chunk.css"
+  },
+  {
+    "revision": "064a0034c48425700cb7",
+    "url": "/cardDealer/static/js/2.0d442ea7.chunk.js"
+  },
+  {
+    "revision": "c6d01cb19213691bc81a274434d00fe7",
+    "url": "/cardDealer/static/js/2.0d442ea7.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ad4ea105d31618343935",
+    "url": "/cardDealer/static/js/main.d90b9fd2.chunk.js"
+  },
+  {
+    "revision": "eed5b919bbdc5708d698",
+    "url": "/cardDealer/static/js/runtime-main.81e98a61.js"
+  }
+]);
