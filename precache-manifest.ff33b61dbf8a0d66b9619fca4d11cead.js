1e18d14555c04a18068cf34edda5f6a4ec69cf9c
David A <david1906@outlook.com>
2020-02-11 20:05:23 -0600

    First

---
+++ precache-manifest.ff33b61dbf8a0d66b9619fca4d11cead.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "84365dd80bcc03609cb0b618d9764a6f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ae69f36342d0ae2121ec",
+    "url": "/static/css/main.b9fc7fa2.chunk.css"
+  },
+  {
+    "revision": "064a0034c48425700cb7",
+    "url": "/static/js/2.0d442ea7.chunk.js"
+  },
+  {
+    "revision": "c6d01cb19213691bc81a274434d00fe7",
+    "url": "/static/js/2.0d442ea7.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ae69f36342d0ae2121ec",
+    "url": "/static/js/main.26248b8c.chunk.js"
+  },
+  {
+    "revision": "ac3a31116d5dd947f108",
+    "url": "/static/js/runtime-main.72216327.js"
+  }
+]);
